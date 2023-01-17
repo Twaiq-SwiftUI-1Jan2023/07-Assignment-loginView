9569b3b76a755947b9637a894e8b8049ef1ed035
amer266030 <amer266030@users.noreply.github.com>
2023-01-17 18:50:27 +0300

    Amer Alyusuf
    Amer Alyusuf - Login View

---
+++ LoginView/LoginView/Constants/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  LoginView
+//
+//  Created by Amer Alyusuf on 17/01/2023.
+//
+
+import SwiftUI
+
+struct Colors {
+    static let captainsBlue = Color(hex: "#577188")
+    static let denim = Color(hex: "#7B8FA1")
+    static let community = Color(hex: "#D0B997")
+    static let prosecco = Color(hex: "#FAD6A5")
+    static let stone = Color(hex: "#C3C3C3")
+    static let green = Color(hex: "#668864")
+    static let dark = Color(hex: "#483C32")
+}
