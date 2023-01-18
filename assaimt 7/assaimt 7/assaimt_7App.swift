a0d49a53cb0601093b04995a3952a6569f3a7fa3
Mansouraldhwyan <94120818+Mansouraldhwyan@users.noreply.github.com>
2023-01-18 09:47:52 +0300

    Add files via upload

---
+++ assaimt 7/assaimt 7/assaimt_7App.swift
@@ -0,0 +1,17 @@
+//
+//  assaimt_7App.swift
+//  assaimt 7
+//
+//  Created by Abudulaziz on 25/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct assaimt_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
