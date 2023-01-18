c1ca50ea1f83d076a76d2aab3713616d6f58b9b4
Judy Alshahrani <judy@alMacBook-Air-alkhas-bJudy.local>
2023-01-18 15:31:03 +0300

    upload

---
+++ HomeWork-UI/HomeWork-UI/HomeWork-UI/HomeWork_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork_UIApp.swift
+//  HomeWork-UI
+//
+//  Created by Judy Alshahrani on 24/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
