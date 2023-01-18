30154e870ca0cf875ca084ebb7a8be387301e7c9
imkh2 <121610164+imkh2@users.noreply.github.com>
2023-01-18 09:51:32 +0300

    Add files via upload

---
+++ ass7/ass7/ass7App.swift
@@ -0,0 +1,17 @@
+//
+//  ass7App.swift
+//  ass7
+//
+//  Created by MAJEDAH on 24/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct ass7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
